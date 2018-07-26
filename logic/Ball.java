b265244dd1c36f3c036466b967dd9cc62340d49b
Willem <whunt1@uvm.edu>
2018-07-26 19:10:48 -0400

    getters, setters, and bounce angle calculator

--- logic/Ball.java
+++ logic/Ball.java
@@ -3,14 +3,44 @@ package logic;
 public class Ball {
 
     public static final int SIZE = 10;
-    private double x;
-    private double y;
+    private int x;
+    private int y;
+    private int dx;
+    private int dy;
+    private double angle = 0;
 
-    public double getX() {
+    public void Bounce(Paddle paddle){
+
+        int paddleCenter    = paddle.getYPos()+Paddle.PADDLE_HEIGHT/2;
+        int ballCenter      = this.getYPos()+Ball.SIZE/2;
+
+        angle = paddleCenter - ballCenter;
+        if(angle > 80)angle = 80;
+        if(angle < -80)angle = -80;
+
+    }
+
+    public int getXPos() {
         return x;
     }
 
-    public double getY() {
+    public int getYPos() {
         return y;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
     }
 }
